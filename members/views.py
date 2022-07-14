da3886fb274ffb83cce4e693c11dc2be9892af04
John Ferlito <74108174+jjfcode@users.noreply.github.com>
2022-07-14 10:58:19 -0400

    2nd

--- members/views.py
+++ members/views.py
@@ -1,7 +1,23 @@
-from re import template
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Members
 
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+  
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+  
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
