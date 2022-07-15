da2290b00b25a31b5655568e64f486e55f3703c7
John Ferlito <74108174+jjfcode@users.noreply.github.com>
2022-07-15 17:48:52 -0400

    3 day

--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+from re import template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
@@ -19,5 +20,27 @@ def addrecord(request):
     x = request.POST['first']
     y = request.POST['last']
     member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
     member.save()
     return HttpResponseRedirect(reverse('index'))
